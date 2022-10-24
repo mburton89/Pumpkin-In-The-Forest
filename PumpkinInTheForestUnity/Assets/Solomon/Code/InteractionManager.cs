82722ad8e2c21c61a60731ddaccf9e18621231a4
WEBSTERAD\wustudent <dts@webster.edu>
2022-10-24 15:55:45 -0500

    Interaction System built.
    Displays a pop-up when the player touches an object with the interaction script.

--- PumpkinInTheForestUnity/Assets/Solomon/Code/InteractionManager.cs
+++ PumpkinInTheForestUnity/Assets/Solomon/Code/InteractionManager.cs
@@ -4,10 +4,56 @@ using UnityEngine;
 
 public class InteractionManager : MonoBehaviour
 {
+    private Sprite defaultIcon = null;
+
+    public GameObject spriteObject;
+    public Sprite Icon = null;
+    private Sprite interactsPrite;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        spriteObject.transform.position = transform.position + new Vector3(0, 10f, 0);
+
+        defaultIcon = Icon;
+        Sprite tempSprite = spriteObject.GetComponent<Sprite>();
+        tempSprite = Icon;
+        interactsPrite = spriteObject.GetComponent<Sprite>();
+        interactsPrite = Icon;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        spriteObject.SetActive(false);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<Interaction>())
+        {
+            GameObject other = collision.gameObject;
+            Interaction otherInteract = other.GetComponent<Interaction>();
+
+            if (otherInteract.GetUseDefaultIcon() == false)
+            {
+                interactsPrite = otherInteract.GetObjectIcon();
+            }
+            else
+            {
+                interactsPrite = defaultIcon;
+            }
+
+            if (otherInteract.GetDisplayOnMe() == false)
+            {
+                spriteObject.transform.position = transform.position + new Vector3(0, 5f, 0);
+            }
+            else
+            {
+                spriteObject.transform.position = other.transform.position + new Vector3(0, 5f, 0);
+            }
+
+            spriteObject.SetActive(true);
+        }
     }
 
     // Update is called once per frame
