6a81c96c0dc324fcc56c71a49028fa6e196a12fc
WEBSTERAD\wustudent <dts@webster.edu>
2022-11-21 15:23:41 -0600

    Fixed an error in the dialogue system

--- PumpkinInTheForestUnity/Assets/Solomon/Code/Game Code/DialogueSystem.cs
+++ PumpkinInTheForestUnity/Assets/Solomon/Code/Game Code/DialogueSystem.cs
@@ -78,6 +78,7 @@ public class DialogueSystem : MonoBehaviour
 
     private bool pumpkinIsPaused = false;
     private bool activatePumpkinWhenFinished = true;
+    private bool simulated = false;
     /* The dialogue Box (Documentation)
      * The dialogue box variables are used to decide which and how many characters are being displayed to the screen at the current moment.
      * There is only one dialogue box as of right now.
@@ -257,6 +258,17 @@ public class DialogueSystem : MonoBehaviour
         {
             simulateButtonPress = false;
 
+            if (!simulated)
+            {
+                if (!pumpkinIsPaused)
+                {
+                    isDialogueFinished = false;
+                    deactivatePumpkin();
+                    print("Pumpkin was de-activated.");
+
+                }
+            }
+
             if ((!canUpdate))
             {
                 canUpdate = true;
@@ -354,14 +366,18 @@ public class DialogueSystem : MonoBehaviour
     public void showText(string text, bool pause = true, string titleText = " ")
     {
         //pumpkin in the forest specific code.
-        if (!pumpkinIsPaused)
+        if (simulated)
         {
-            isDialogueFinished = false;
-            deactivatePumpkin();
-            print("Pumpkin was de-activated.");
+            if (!pumpkinIsPaused)
+            {
+                isDialogueFinished = false;
+                deactivatePumpkin();
+                print("Pumpkin was de-activated.");
 
+            }
+            //
         }
-        //
+
 
         isDialogueFinished = false;
 
@@ -456,14 +472,18 @@ public class DialogueSystem : MonoBehaviour
 
     public void showTextWithImage(string text, bool pause = true, string titleText = " ", string characterImage = "Default")
     {
-        //pumpkin in the forest specific code.
-        if (!pumpkinIsPaused)
+        if (simulated == true)
         {
-            isDialogueFinished = false;
-            deactivatePumpkin();
-            print("Pumpkin was de-activated.");
+            if (!pumpkinIsPaused)
+            {
+                isDialogueFinished = false;
+                deactivatePumpkin();
+                print("Pumpkin was de-activated.");
 
+            }
         }
+        //pumpkin in the forest specific code.
+        
         //
 
 
@@ -564,6 +584,7 @@ public class DialogueSystem : MonoBehaviour
 
     public bool isFinished()
     {
+        simulated = false;
         return isDialogueFinished;
     }
 
@@ -576,6 +597,7 @@ public class DialogueSystem : MonoBehaviour
     public void simulate()
     {
         simulateButtonPress = true;
+        simulated = true;
     }
 
     //Pumpkin in the forest specific code.
